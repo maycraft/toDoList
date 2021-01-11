59348ac9a33c80069b16287bafaa94825b8a065e
cantwell <rjazantsev@gmail.com>
2021-01-11 10:00:02 +0300

    adding starting App files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+        <div className="main">
+          <h1>toDoList</h1>    
+          <form>
+              <input type="text" 
+                      name="task" 
+                      placeholder="Описание" 
+                      className="field"/>
+              <button type="submit" className="btn">Добавить</button>
+          </form>
+          <ul className="items">
+            <li className="itemTask">
+              <div className="inputBlock">
+                  <input type="checkbox"
+                          className="customCheckbox" 
+                          id="3" 
+                          />
+                  <label htmlFor="3">Тест</label>
+              </div>
+              <span className="close">x</span>
+          </li>
+          </ul>
+        </div>
     </div>
-  );
+  )
 }
 
 export default App;
